2ee7811830e7d9689e9d6f466a9fcdf100499707
KevinBurgin <Kevinburgin33@gmail.com>
2021-05-16 16:02:45 -0700

    completed assessment

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -16,22 +16,44 @@
 // --------------------1) Create a function that takes a number as an argument and decides if the number is evenly divisble by three or not.
 
 // a) Create a test with expect statements for each of the variables provided.
-
-var num1 = 15
+//Create a test called divisibleBy
+describe("divisibleBy", () => {
+//Test variables if it is divisible by 3
+    test("is divisable by three", () => {
+        let num1 = 15
+        let num2 = 0
+        let num3 = -7
+//Expect variable is or is not divisible by 3
+        expect(divisibleBy(num1, 3)).toEqual("15 is divisible by 3");
+        expect(divisibleBy(num2, 3)).toEqual("0 is divisible by 3");
+        expect(divisibleBy(num3, 3)).toEqual("-7 is not divisible by 3");
+    });
+});
+ var num1 = 15
 // Expected output: "15 is divisible by three"
 
-var num2 = 0
+ var num2 = 0
 // Expected output: "0 is divisible by three"
 
-var num3 = -7
+ var num3 = -7
 // Expected output: "-7 is not divisble by three"
 
 
 
-
-
 // b) Create the function that makes the test pass.
-
+//Create a function the decides if a variable is divisible by 3
+const divisibleBy = (int) => {
+//Write a conditional statement that decides if a variable is divisible 3 if yes return var is divisible by 3
+    if(int % 3 === 0){
+    return `${int} is divisible by 3`
+//if not divisble by 3 return var is not divisible by 3
+}  else if(int % 3 !== 0){
+    return `${int} is not divisible by 3`
+//Write a catch all for errors
+}  else {
+    return "error"
+}
+}
 
 
 
@@ -39,6 +61,17 @@ var num3 = -7
 // --------------------2) Create a function that takes in an array of words and returns an array with all the words capitalized.
 
 // a) Create a test with expect statements for each of the variables provided.
+//create a test for a function that will return the first letter in each word in array capitalized
+describe ("capitalized", () => {
+    let randomNouns1 = ["streetlamp", "potato", "teeth", "conclusion", "nephew"]
+    let randomNouns2 = ["temperature", "database", "chopsticks", "mango", "deduction"]
+//test array to return capital letter
+    test("return array in capitalized", () => {
+//expect the first letter of each string in the array to be capitalized
+        expect(capitalized(randomNoun1)).toEqual("Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew")
+        expect(capitalized(randomNoun2)).toEqual("Temperature", "Database", "Chopsticks", "Mango", "Deduction")
+    })
+})
 
 var randomNouns1 = ["streetlamp", "potato", "teeth", "conclusion", "nephew"]
 // Expected output: ["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew"]
@@ -48,17 +81,34 @@ var randomNouns2 = ["temperature", "database", "chopsticks", "mango", "deduction
 
 
 
-
-
 // b) Create the function that makes the test pass.
-
-
-
+//create a function that will return the first letter in each word in array capitalized
+const capitalized = (array) => {
+//intiate a variable for an array
+   var newArray = []
+//for the length of the variable
+   for(var i = 0; i < array.length; i++){
+//locate index 0 and uppercase then push to new array
+       newArray.push(array[i].charAt(0).toUpperCase() + array[i].slice(1))
+   }
+//join and return new array
+   return newArray.join(' ')
+}
 
 
 // --------------------3) Create a function that takes in a string and logs the index of the first vowel.
 
 // a) Create a test with expect statements for each of the variables provided.
+//Create a test that will check a function that logs the index of the first vowel
+describe("firstVowelIndex", () => {
+//test string to return first vowel in string
+    test("return and logs first vowel index", () =>{
+//expect a return of first vowel in string
+        expect(firstVowelIndex(vowelTester1)).toEqual(1)
+        expect(firstVowelIndex(vowelTester2)).toEqual(0)
+        expect(firstVowelIndex(vowelTester3)).toEqual(2)
+    })
+})
 
 var vowelTester1 = "learn"
 // Expected output: 1
@@ -71,4 +121,20 @@ var vowelTester3 = "challenge"
 
 
 
-// b) Create the function that makes the test pass.
+// b) Create the function that makes the test pass
+//Create a function that takes a string and returns the first vowel
+const firstVowelIndex = (string) => {
+//initiate a variable that equals vowels both lowercase and uppercase
+    let vowelList = 'aeiouAEIOU'
+//initate another variable set to 0
+    let vowelIndex = 0
+//for the length of the string
+    for(var i= 0; i < string.length ; i++)
+    {
+//write a conditional that finds the index of the first vowel
+        if (vowelList.indexOf(string[i]) !== -1)
+    vowelIndex += 1
+    }
+//return the vowelindex
+return vowelIndex
+}
--- interview-questions.md
+++ interview-questions.md
@@ -8,58 +8,55 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. The built in method .map() takes predefined parameters. What are they? Which are required and which are optional?
 
-  Your answer:
+  Your answer: The method .map() takes three possible parameters which are: values, index, and array. They are all required.
 
-  Researched answer:
+  Researched answer: The built in method .map() takes in three possible parameters which are: values, index, and array. Value, however, is an optional parameter and index and array are a required parameter. 
 
 
 
 2. What is object destructuring?
 
-  Your answer:
+  Your answer: Object destructuring is when you create a shortcut to get the values of a nested object
 
-  Researched answer:
+  Researched answer: Object destructuring is when you create a shortcut that allows us to break down data from arrays , objects, and maps and set them in new distinct variables.
 
 
 
 3. Why would you use the method super()?
 
-  Your answer:
+  Your answer: You would use the method super() when creating a class inheritance. Super() refers to a parent class and used in the child class. 
 
-  Researched answer:
+  Researched answer: You would use the method super() in a child class to call the constructor of a parent class to access the parent class's properties and methods.
 
 
 
 4. What is a React Fragment? Why would you use one?
 
-  Your answer:
+  Your answer: React Fragements are a useful tool in react to list multiple elements.
 
-  Researched answer:
-
+  Researched answer: React Fragments allow us to list multiple elements without having to add an extra node to the DOM. You might use one when listing child elements in a parent class.
 
 
 5. What is JSX?
 
-  Your answer:
+  Your answer: JSX is an extension to Javascript that makes it easier to work in react
 
-  Researched answer:
+  Researched answer: JSX is an extension syntax to Javascript used to write HTML and script together in React.
 
 
 
 6. STRETCH: What is hoisting in JavaScript?
 
-  Your answer:
+  Your answer: 
 
-  Researched answer:
-
-
+  Researched answer: Hoisting in JavaScript allows us to move variables and functions to the top of their scope before coding execution.
 
 ## Looking Ahead: Terms for Next Week
 
 Research and define the following terms to the best of your ability.
 
-1. React props:
-2. React lifecycle methods:
-3. Logic/smart component:
-4. Display/dumb component:
-5. DOM Events:
+1. React props: React props are used to pass data between React components.
+2. React lifecycle methods: Every component in React goes a through a lifecycle starting with the birth of your component(mounting), then the growth of your component(update), and the death of your component(Unmount). 
+3. Logic/smart component: A Logic/ smart component ia any component that manages its own state.
+4. Display/dumb component: A Display/dumb component is a stateless component that presents something to the DOM once and then the component is done with it.
+5. DOM Events: DOM events are actions that the user does or a result of state change of the elements in a DOM tree.
